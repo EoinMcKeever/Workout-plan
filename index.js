e5863625a12a5fe00190bb700e3df70ad1a0a10c
Eoin Mc Keever <eoin.mckeever1999@gmail.com>
2023-01-22 21:50:05 +0000

    added slider images

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-$('.nav').click(function() {
-    var id = $(this).attr('id');
-    $('html, body').animate({
-        scrollTop: ($('#' + id + '.section').offset().top)
-    }, 1000);
-});
+let lowerWorkout = ["squat", "leg press", "quad extensions", "hamstring extensions", "calf raises"]
+let upperWorkout = []
+let shortFlexibility =[]
+
+const buttons = document.querySelectorAll('.btn');
+var currentPage = document.getElementById("testimonials")
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', function handleClick(event) {
+
+        var newPage = document.getElementById(this.name);
+        newPage.style.display = "block"
+        currentPage.style.display = "none"
+        currentPage = newPage
+    
+    }
+)})
+
+// document.querySelectorAll(".btn").addEventListener("click", function(event) {
+//     alert("hell")
+// })
+
+
+// $('button').click(function() {
+//     var id = $(this)
+//     alert("hell")
+// })
