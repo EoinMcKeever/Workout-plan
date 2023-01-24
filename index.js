7115a6ac934e7fb9b9a85f1380b3453ba682eeed
Eoin Mc Keever <eoin.mckeever1999@gmail.com>
2023-01-24 20:50:09 +0000

    added back button and finished carousel

--- index.js
+++ index.js
@@ -4,15 +4,33 @@ let shortFlexibility =[]
 
 const buttons = document.querySelectorAll('.btn');
 var currentPage = document.getElementById("testimonials")
+var lastPage = document.getElementById("testimonials")
+var lastPageList = []
 
 buttons.forEach(btn => {
     btn.addEventListener('click', function handleClick(event) {
+        console.log(this.name);
 
-        var newPage = document.getElementById(this.name);
-        newPage.style.display = "block"
-        currentPage.style.display = "none"
-        currentPage = newPage
-    
+        if(this.name === "go-back") {
+            if(lastPageList.length === 0) {
+                currentPage.style.display = "none";
+                lastPage.style.display = "block"
+                currentPage = lastPage;
+            } else {
+                currentPage.style.display = "none";
+                currentPage = lastPageList[lastPageList.length-1]
+                currentPage.style.display = "block"
+                lastPageList.pop()
+            }
+
+        } else {
+            var newPage = document.getElementById(this.name);
+            newPage.style.display = "block"
+            currentPage.style.display = "none"
+            lastPageList.push(currentPage);
+            currentPage = newPage;
+        }
+        console.log(lastPageList.length);
     }
 )})
 
